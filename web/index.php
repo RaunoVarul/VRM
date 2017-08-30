1990405fdf50f85cb062f0fc688b30dc60cd1b19
Rauno Varul <rauno.varul@khk.ee>
2017-08-30 12:32:40 +0300

    Created database connection

--- web/index.php
+++ web/index.php
@@ -4,10 +4,20 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+//Definitions
 $app['debug'] = true;
 
+//Using Twig template Framework
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__.'/../views',
+]);
+
+//using Doctrine DBal
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+	'db_options'=> [
+	'driver' => 'pdo_sqlite',
+	'path' => __DIR__.'/../database/app.db',
+	],
 ]);
 
 $app->get('/bookings/create', function () use ($app) {
