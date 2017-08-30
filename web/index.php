7971a8ac9e8858d093a7b79d105de34dd6851357
Rauno Varul <rauno.varul@khk.ee>
2017-08-30 12:45:58 +0300

    Created bookings table

--- web/index.php
+++ web/index.php
@@ -14,11 +14,30 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [
 
 //using Doctrine DBal
 $app->register(new Silex\Provider\DoctrineServiceProvider(), [
-	'db_options'=> [
+	'db.options'=> [
 	'driver' => 'pdo_sqlite',
 	'path' => __DIR__.'/../database/app.db',
 	],
 ]);
+
+//Create table bookings
+if (!$app['db']->getSchemaManager()->tablesExist('bookings')) {
+    $app['db']->executeQuery("CREATE TABLE bookings (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstName VARCHAR(40) NOT NULL,
+        lastName VARCHAR(40) NOT NULL,
+        phone VARCHAR(10) NOT NULL,
+        email VARCHAR(20) DEFAULT NULL,
+        birthday DATE NOT NULL,
+        startDate DATE NOT NULL,
+        endDate DATE NOT NULL,
+        arrivalTime TIME DEFAULT NULL,
+        additionalInformation TEXT,
+        nrOfPeople INT NOT NULL,
+        payingMethod VARCHAR(10) NOT NULL
+    );");
+}
+
 
 $app->get('/bookings/create', function () use ($app) {
     return $app['twig']->render('base.html.twig');
